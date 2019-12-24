8ad1bdafc04f6648e31ef1cff12246957c53e564
Twan <twan.3ssen@gmail.com>
2019-12-24 13:29:18 +0100

    ObjectOriented TicTactToe

--- boterkaaseneieren.py
+++ boterkaaseneieren.py
@@ -1,65 +1,121 @@
-from random import randrange
-import os
+# TicTacToe
+import sys, os, random, collections
 
-def play():
-    def printboard():
+class Board():
+    board  = { }
+    winner = False
+
+    def __init__(self):
+        # Create and show the playingboard
+        for i in (range(9)):
+            self.board[i+1] = i+1
+        self.show()
+
+    def show(self):
+        # Show the playingboard in it's current state
         os.system('clear')
-        print("Here's the board:\n")
-        for e in (2, 1, 0):
-            print(f"{b[e*3+1]}|{b[e*3+2]}|{b[e*3+3]}")
-            print("-+-+-") if e else print("")
+        for i in (2, 1, 0):
+            print(f"{self.board[i*3+1]}|{self.board[i*3+2]}|{self.board[i*3+3]}")
+            print("-+-+-") if i else print("")
 
-    def choose_beginner():
-        x_or_o = randrange(2)
-        return "X" if x_or_o == 1 else "O"
-
-    def player_input(turn):
-        def choice():
-            while True:
-                c = input(f'Player {turn}, place your {turn}: ')
-                try:
-                    int(c); return int(c)
-                except ValueError:
-                    wrong_input(); continue
-
-        s = choice()
-        while not 1 <= s <= 9:
-            wrong_input()
-            s = choice()
-        else:
-            return s
-
-    def wrong_input():
-        printboard()
-        print("Between 1 and 9...")
-
-    def check_for_winner():
+    def check_for_winner(self):
+        # See if we have a winner yet
         winning = [
             [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
             [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3]
         ]
         for i in winning:
-            if b[i[0]] == b[i[1]] == b[i[2]]:
-                return True
+            if self.board[i[0]] == self.board[i[1]] == self.board[i[2]]:
+                self.winner = True
 
-    b = {}
-    for i in (range(9)):
-        b[i+1] = i+1
+    def place_marker(self, turn, location):
+        # Place an 'X' or 'O' on the board
+        if (self.board[location] not in {"X", "O"}):
+            self.board[location] = turn
+            return True
+        else:
+            return False
 
-    winner = False
-    turn   = choose_beginner()
+class PlayerTurn():
+    turn = None
 
-    while not winner:
-        printboard()
-        location = player_input(turn)
-        if (b[location] not in {"X", "O"}):
-            b[location] = turn
-            if check_for_winner():
-                winner = turn
+    def __init__(self):
+        # Randomly choose who starts
+        self.turn = "X" if random.randrange(2) == 1 else "O"
+
+    def next_turn(self):
+        # Swap turns
+        self.turn = "X" if self.turn == "O" else "O"
+        return self.turn
+
+
+class PlayerInput():
+    def request_input(self, turn):
+        # Validate the player's pick
+        while True:
+            s = self.choice(turn)
+            if not 1 <= s <= 9:
+                self.wrong_input()
+            else:
                 break
-            turn = "X" if turn == "O" else "O"
 
-    printboard(); print(f"{winner} wins this game!")
+        return s
 
+    def choice(self, turn):
+        # Ask the player to take their pick
+        while True:
+            pick = input(f'Player {turn}, place your {turn}: ')
+            try:
+                int(pick)
+                return int(pick)
+            except ValueError:
+                self.wrong_input()
+                continue
+
+    def wrong_input(self):
+        print("Between 1 and 9...")
+
+
+def play():
+    score_count = collections.defaultdict(int)
+
+    while True:
+        board   = Board()
+        p_turn  = PlayerTurn()
+        p_input = PlayerInput()
+
+        for i in (range(9)):
+            while True:
+                mark_placed = board.place_marker(
+                    p_turn.turn,
+                    p_input.request_input(p_turn.turn)
+                )
+                if (mark_placed == True):
+                    break
+                else:
+                    print("You can't do that...")
+
+            board.check_for_winner()
+            board.show()
+            if not board.winner:
+                p_turn.next_turn()
+            else:
+                print(f"{p_turn.turn} wins this game!")
+                score_count[p_turn.turn] += 1
+                break
+
+        if not board.winner:
+            print("Nobody wins.")
+
+        again = input("Play again? (y/n) ")
+        if (again.lower() in "yes"):
+            True
+        else:
+            break
+
+    print(dict(score_count))
+    sys.exit("Thanks for playing!")
+
+
+# Let's play!
 play()
-
